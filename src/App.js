75440743febf915c93041a20911c090ca5883795
unknown <aluminaze@gmail.com>
2020-07-22 03:44:18 +0200

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import style from "./App.module.css"
 import Header from "./components/Header/Header";
 
 const App = (props) => {
   return (
-    <Header />
+    <div className={style.container}>
+      <Header />
+      <div className={style.wrapper}>
+        <div className={style.documents__tree}>Source Tree</div>
+        <div className={style.documents__viewer}>
+          <div className={style.documents__text}>Documents text</div>
+          <div className={style.documents__infoBar, style.infoBar}>
+            <div className={style.infoBar__openFileName}>Name of document</div>
+            <div className={style.infoBar__currentDate}>01/01/2020</div>
+          </div>          
+        </div>
+      </div>
+    </div>    
   )
 }
 
