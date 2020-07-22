ff296d05fd1585ac3ead6d926e0ecbaef46f7f8a
unknown <aluminaze@gmail.com>
2020-07-22 15:02:48 +0200

    Updates

---
+++ precache-manifest.b927b9c0563434dfe22657e8eed10006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816c9cc86849199d8ad1c4a97bd7c4ff",
+    "url": "/test-project/index.html"
+  },
+  {
+    "revision": "3650c2054b75f140325e",
+    "url": "/test-project/static/css/main.bce46464.chunk.css"
+  },
+  {
+    "revision": "42bd9ad9d2432971ed4f",
+    "url": "/test-project/static/js/2.88d8d3cc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-project/static/js/2.88d8d3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3650c2054b75f140325e",
+    "url": "/test-project/static/js/main.917ba664.chunk.js"
+  },
+  {
+    "revision": "e893b0a89f2d626c5752",
+    "url": "/test-project/static/js/runtime-main.16e8256d.js"
+  }
+]);
