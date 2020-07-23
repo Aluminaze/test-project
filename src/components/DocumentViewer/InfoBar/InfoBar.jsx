eaa2f5571b96bf3510fcb996aab795a0afdee143
unknown <aluminaze@gmail.com>
2020-07-23 16:00:56 +0200

    add getCurrentDate function for InfoBar

--- src/components/DocumentViewer/InfoBar/InfoBar.jsx
+++ src/components/DocumentViewer/InfoBar/InfoBar.jsx
@@ -3,13 +3,22 @@ import style from "./InfoBar.module.css";
 import DescriptionIcon from "@material-ui/icons/Description";
 
 const InfoBar = (props) => {
+  const getCurrentDate = () => {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  };
+
   return (
     <div className={(style.documents__infoBar, style.infoBar)}>
       <div className={style.infoBar__openFileName}>
         <DescriptionIcon fontSize="small" />
-        {props.documentIsOpen_name !== null ?  props.documentIsOpen_name : null}
+        {props.documentIsOpen_name !== null ? props.documentIsOpen_name : null}
       </div>
-      <div className={style.infoBar__currentDate}>01/01/2020</div>
+      <div className={style.infoBar__currentDate}>{getCurrentDate()}</div>
     </div>
   );
 };
