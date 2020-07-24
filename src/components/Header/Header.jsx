a5046d44398862d9b118a962855d928f9319400c
unknown <aluminaze@gmail.com>
2020-07-24 15:18:31 +0200

    refactoring Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,86 +1,16 @@
 import React from "react";
 import style from "./Header.module.css";
 import AccountBoxRoundedIcon from "@material-ui/icons/AccountBoxRounded";
-import SaveRoundedIcon from "@material-ui/icons/SaveRounded";
-import EditRoundedIcon from "@material-ui/icons/EditRounded";
 
 const Header = (props) => {
-  let editButton;
-  const editButton_active = (
-    <button
-      className={style.header__editButton}
-      onClick={(event) => {
-        event.preventDefault();
-        props.startEditDocument(props.documentIsOpen[0].text);
-      }}
-    >
-      <EditRoundedIcon
-        className={style.header__openIcon}
-        style={{ fill: "white" }}
-        fontSize="default"
-      />
-      <span>Редактировать</span>
-    </button>
-  );
-  const editButton_deactive = (
-    <button disabled className={style.header__editButton__disabled}>
-      <EditRoundedIcon
-        className={style.header__openIcon}
-        style={{ fill: "#a6a6a6" }}
-        fontSize="default"
-      />
-      <span>Редактировать</span>
-    </button>
-  );
-  let saveButton;
-  const saveButton_active = (
-    <button
-      className={style.header__saveButton}
-      onClick={() =>
-        props.documentEditMode
-          ? props.saveEditDocumentText(props.selectedDataId)
-          : null
-      }
-    >
-      <SaveRoundedIcon
-        className={style.header__saveIcon}
-        style={{ fill: "white" }}
-        fontSize="default"
-      />
-      <span>Сохранить</span>
-    </button>
-  );
-  const saveButton_deactive = (
-    <button disabled className={style.header__saveButton__disabled}>
-      <SaveRoundedIcon
-        className={style.header__saveIcon}
-        style={{ fill: "#a6a6a6" }}
-        fontSize="default"
-      />
-      <span>Сохранить</span>
-    </button>
-  );
-
-  if (props.documentEditMode) {
-    saveButton = saveButton_active;
-    editButton = editButton_deactive;
-  } else {
-    saveButton = saveButton_deactive;
-    if (props.selectedDataId !== null) {
-      editButton = editButton_active;
-    } else {
-      editButton = editButton_deactive;
-    }
-  }
-
   return (
     <section className={style.header}>
       <div className={style.header__leftSide}>
         <h1 className={style.header__title}>Тестовое задание</h1>
       </div>
       <div className={style.header__rightSide}>
-        {saveButton}
-        {editButton}
+        {props.saveButton}
+        {props.editButton}
         <AccountBoxRoundedIcon
           className={style.header__accountIcon}
           style={{ fill: "white" }}
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,27 +1,85 @@
 import React from "react";
+import style from "./Header.module.css";
+import SaveRoundedIcon from "@material-ui/icons/SaveRounded";
+import EditRoundedIcon from "@material-ui/icons/EditRounded";
+import Header from "./Header";
 import {
   getStartEditDocument,
   saveEditDocumentText,
 } from "../../redux/documentViewerReducer";
 import { connect } from "react-redux";
-import Header from "./Header";
 
 const HeaderContainer = (props) => {
-  const startEditDocument = (currentOpenDocumentText) => {
-    if (props.documentIsOpen[0].name !== null) {
-      props.getStartEditDocument(currentOpenDocumentText);
-    }
-  };
+  const startEditDocument = (currentOpenDocumentText) =>
+    props.documentIsOpen[0].name !== null
+      ? props.getStartEditDocument(currentOpenDocumentText)
+      : null;
 
-  return (
-    <Header
-      startEditDocument={startEditDocument}
-      saveEditDocumentText={props.saveEditDocumentText}
-      documentEditMode={props.documentEditMode}
-      selectedDataId={props.selectedDataId}
-      documentIsOpen={props.documentIsOpen}
-    />
+  let editButton;
+  const editButton_active = (
+    <button
+      className={style.header__editButton}
+      onClick={(event) => {
+        event.preventDefault();
+        startEditDocument(props.documentIsOpen[0].text);
+      }}
+    >
+      <EditRoundedIcon
+        style={{ fill: "white" }}
+        fontSize="default"
+      />
+      <span>Редактировать</span>
+    </button>
   );
+  const editButton_deactive = (
+    <button disabled className={style.header__editButton__disabled}>
+      <EditRoundedIcon
+        style={{ fill: "#a6a6a6" }}
+        fontSize="default"
+      />
+      <span>Редактировать</span>
+    </button>
+  );
+  let saveButton;
+  const saveButton_active = (
+    <button
+      className={style.header__saveButton}
+      onClick={() =>
+        props.documentEditMode
+          ? props.saveEditDocumentText(props.selectedDataId)
+          : null
+      }
+    >
+      <SaveRoundedIcon
+        style={{ fill: "white" }}
+        fontSize="default"
+      />
+      <span>Сохранить</span>
+    </button>
+  );
+  const saveButton_deactive = (
+    <button disabled className={style.header__saveButton__disabled}>
+      <SaveRoundedIcon
+        style={{ fill: "#a6a6a6" }}
+        fontSize="default"
+      />
+      <span>Сохранить</span>
+    </button>
+  );
+
+  if (props.documentEditMode) {
+    saveButton = saveButton_active;
+    editButton = editButton_deactive;
+  } else {
+    saveButton = saveButton_deactive;
+    if (props.selectedDataId !== null) {
+      editButton = editButton_active;
+    } else {
+      editButton = editButton_deactive;
+    }
+  }
+
+  return <Header saveButton={saveButton} editButton={editButton} />;
 };
 
 const mapStateToProps = (state) => {
